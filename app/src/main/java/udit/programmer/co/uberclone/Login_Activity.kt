4580d89f9c3daee8bfdc17f83b600aa3df43a6bf
Udit Jain <jain30udit@gmail.com>
2020-05-09 16:18:03 +0530

    Shared Preferences stuff added

--- app/src/main/java/udit/programmer/co/uberclone/Login_Activity.kt
+++ app/src/main/java/udit/programmer/co/uberclone/Login_Activity.kt
@@ -1,9 +1,11 @@
 package udit.programmer.co.uberclone
 
+import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import androidx.core.content.edit
 import com.google.android.material.snackbar.Snackbar
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.FirebaseDatabase
@@ -15,7 +17,6 @@ import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.layout_signin.*
 
 class Login_Activity : AppCompatActivity() {
-
     val auth by lazy {
         FirebaseAuth.getInstance()
     }
@@ -25,13 +26,26 @@ class Login_Activity : AppCompatActivity() {
     val users by lazy {
         db.getReference("Users")
     }
-
+    lateinit var username: String
+    lateinit var password: String
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login_)
 
+        val sharedPreferences = getSharedPreferences("999", Context.MODE_PRIVATE)
+        email_et_signin.setText(sharedPreferences.getString("username", "USERNAME"))
+        password_et_signin.setText(sharedPreferences.getString("password", "PASSWORD"))
+
         login_btn.setOnClickListener(object : View.OnClickListener {
             override fun onClick(v: View?) {
+                if (email_et_signin.text.toString().isNotEmpty()) {
+                    username = email_et_signin.text.toString()
+                }
+                if (password_et_signin.text.toString().isNotEmpty() || password_et_signin.text.toString().length < 6) {
+                    password = password_et_signin.text.toString()
+                }
+                sharedPreferences.edit { putString("username", username) }
+                sharedPreferences.edit { putString("password", password) }
 
                 login_btn.isEnabled = false
                 var loading_dialog =
@@ -57,7 +71,5 @@ class Login_Activity : AppCompatActivity() {
             }
 
         })
-
-
     }
 }
